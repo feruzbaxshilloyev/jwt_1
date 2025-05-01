d357bc0dc1a10be6452b1fb1420c7b3cab1c539e
feruzbaxshilloyev <baxshilloyevferuz23@gmail.com>
2025-05-01 15:49:53 +0500

    jwt

---
+++ app/views.py
@@ -0,0 +1,24 @@
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from .serializers import RegisterSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+class LogoutView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response({"detail": "Chiqildi"}, status=200)
+        except Exception:
+            return Response({"error": "Xatolik"}, status=400)
